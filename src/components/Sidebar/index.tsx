0c807d03522638a852b818ad62d8e9b00188b98b
enesakkose <ens.akkose@gmail.com>
2023-05-29 20:12:47 +0300

    key add

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -8,10 +8,10 @@ function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       {SidebarTabs.map((tabs) => (
-        <ul className={styles.tabList}>
+        <ul key={tabs.title} className={styles.tabList}>
           <h4 className={styles.title}>{tabs.title}</h4>
           {tabs.tabs.map((tab) => (
-            <li className={styles.tab}>
+            <li key={tab.title} className={styles.tab}>
               <Button asChild>
                 <Link href={tab.href}>{tab.title}</Link>
               </Button>
