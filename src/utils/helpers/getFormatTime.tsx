7486cc0c7f0dfcc04296c47e0ea762db0bce9d4b
enesakkose <ens.akkose@gmail.com>
2023-05-29 20:17:02 +0300

    remove file

--- src/utils/helpers/getFormatTime.tsx
+++
@@ -1,9 +0,0 @@
-import dayjs from 'dayjs'
-
-export const getFormatTime = (time: number) => {
-  return dayjs.unix(time).format('HH:mm')
-}
-
-export const getDate = (time: number) => {
-  return dayjs.unix(time).format('DD.MM.YYYY')
-} 
